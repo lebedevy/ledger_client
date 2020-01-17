e0060d45861dc49a997ab384a0de09af6473ef56
Yury Lebedev <y.d.lebedev@gmail.com>
2020-01-17 09:39:25 -0500

    Bar chart labels and formatting

--- src/components/AggregateOverview/OverviewDetailsTrends.js
+++ src/components/AggregateOverview/OverviewDetailsTrends.js
@@ -12,11 +12,17 @@ const useStyles = makeStyles({
     svg: {
         alignSelf: 'center',
         width: '80vw',
-        maxWidth: '800px',
-        maxHeight: '200px',
+        maxWidth: '99%',
+        height: '200px',
+        maxHeight: '300px',
         // transform: 'rotate(-0.25turn)',
         // background: 'yellow',
-        border: '1px solid #000000',
+        border: '1px solid #00000030',
+        borderRadius: '3px',
+        fontSize: '10px',
+    },
+    chartGrid: {
+        stroke: '#000000',
     },
 });
 
@@ -49,7 +55,7 @@ function OverviewDetailsTrends({ selected, type, start, end }) {
             const data = await res.json();
             console.log(data);
             console.log(endDate.slice(5, 7));
-            const formatted = formatData(data.expenses, parseInt(endDate.slice(5, 7)) - 1);
+            const formatted = formatData(data.expenses, startDate);
             setData(formatted);
             return data;
         }
@@ -59,12 +65,16 @@ function OverviewDetailsTrends({ selected, type, start, end }) {
     function formatData(data, start) {
         let formatted = [];
         let max = 0;
-        if (start < 0) start = 11;
-        console.log(start);
+        let startMonth = parseInt(start.slice(5, 7)) - 1;
+        let year = parseInt(start.slice(0, 4));
+        console.log('Start year', year);
+        if (startMonth < 0) startMonth = 11;
         for (let i = 0, ind = 0; i < 12; i++) {
-            let month = i + start >= 12 ? i + start - 12 : i + start;
+            let month = i + startMonth >= 12 ? i + startMonth - 12 : i + startMonth;
             let monthItem = {};
-            // console.log(months[month]);
+            monthItem.month = months[month];
+            monthItem.year = (year % 100) + '';
+            if (monthItem.month === 'Dec') year++;
             if (data[ind] && parseInt(data[ind]['txn_date'].slice(5, 7)) - 1 === month) {
                 monthItem['amount'] = data[ind]['amount'];
                 max = data[ind]['amount'] > max ? data[ind]['amount'] : max;
@@ -72,12 +82,20 @@ function OverviewDetailsTrends({ selected, type, start, end }) {
             } else {
                 monthItem['amount'] = 0;
             }
-            monthItem.month = months[month];
+
             formatted.push(monthItem);
         }
+        // Make sure 6 divides evenly into max to allow for round grid line labels on the barchart
+        if (max % 6 !== 0) max += 6 - (max % 6);
         setMax(max);
         return formatted;
     }
+
+    // Bar chart positioning variables
+    const topOffset = 15;
+    const barHeight = 70;
+    const rightOffset = 3;
+    const barWidth = 6;
 
     return (
         <div className={classes.container}>
@@ -87,19 +105,86 @@ function OverviewDetailsTrends({ selected, type, start, end }) {
             </h3>
             {data ? null : <CircularProgress style={{ alignSelf: 'center', margin: '10px' }} />}
             <svg className={classes.svg}>
+                {(() => {
+                    let gridlines = [];
+                    for (let i = 0; i < 6; i++) {
+                        let y = topOffset + i * (barHeight / 6);
+                        let label = max - i * (max / 6);
+                        gridlines.push(
+                            <text key={'amountLabel' + i} x="0.5%" y={`${y + 2}%`}>
+                                {label >= 1000
+                                    ? (Math.ceil(label / 100) / 10).toFixed(1) + 'k'
+                                    : label}
+                            </text>,
+                            <line
+                                key={'gridLine' + i}
+                                className={classes.gridLines}
+                                y1={`${y}%`}
+                                y2={`${y}%`}
+                                x1="98.5%"
+                                x2={`${rightOffset - 0.5}%`}
+                                stroke="#00000035"
+                            />
+                        );
+                    }
+                    return gridlines;
+                })()}
+                <line
+                    y1={`${topOffset + 6 * (barHeight / 6)}%`}
+                    y2={`${topOffset + 6 * (barHeight / 6)}%`}
+                    x1="98.5%"
+                    x2={`${rightOffset - 0.5}%`}
+                    stroke="#00000090"
+                />
+                <line
+                    y1={`${5 + 6 * (80 / 6)}%`}
+                    y2="5%"
+                    x1={`${rightOffset - 0.5}%`}
+                    x2={`${rightOffset - 0.5}%`}
+                    stroke="#00000090"
+                />
                 {data
                     ? data.map((el, ind) => (
-                          <React.Fragment>
+                          <React.Fragment key={el.month + 'container'}>
+                              {/* The first rec is to catch hover events for small rects*/}
+                              <rect
+                                  key={el.month + 'barBackup'}
+                                  x={`${rightOffset + 8 * ind}%`}
+                                  y={`${topOffset}%`}
+                                  width={`${barWidth}%`}
+                                  height={`${barHeight}%`}
+                                  style={{ fill: '#00000000' }}
+                              />
                               <rect
                                   key={el.month + 'bar'}
-                                  x={`${2 + 8 * ind}%`}
-                                  y={`${90 - 80 * (el.amount / max)}%`}
-                                  width={'6%'}
-                                  height={`${80 * (el.amount / max)}%`}
-                                  style={{ fill: '#00000090' }}
+                                  x={`${rightOffset + 8 * ind}%`}
+                                  y={`${topOffset + barHeight - barHeight * (el.amount / max)}%`}
+                                  width={`${barWidth}%`}
+                                  height={`${barHeight * (el.amount / max)}%`}
+                                  style={{ fill: '#F75C03' }}
                               />
-                              <text key={el.month + 'label'} x={`${2 + 8 * ind}%`} y={`${100}%`}>
-                                  {el.month}
+                              <text
+                                  key={el.month + 'amountLabel'}
+                                  x={`${rightOffset + barWidth / 2 + 8 * ind}%`}
+                                  y={`${topOffset +
+                                      barHeight -
+                                      barHeight * (el.amount / max) -
+                                      3}%`}
+                                  textAnchor="middle"
+                              >
+                                  {`$${
+                                      el.amount >= 1000
+                                          ? (el.amount / 1000).toFixed(2) + 'k'
+                                          : Math.round(el.amount)
+                                  }`}
+                              </text>
+                              <text
+                                  key={el.month + 'label'}
+                                  x={`${rightOffset + barWidth / 2 + 8 * ind}%`}
+                                  y={`${95}%`}
+                                  textAnchor="middle"
+                              >
+                                  {`${el.month} ${el.year}`}
                               </text>
                           </React.Fragment>
                       ))
@@ -111,7 +196,8 @@ function OverviewDetailsTrends({ selected, type, start, end }) {
 
 const mapStateToProps = state => {
     const { start, end } = state.date.period;
-    return { start, end };
+    const { mobile } = state.screen;
+    return { start, end, mobile };
 };
 
 export default connect(mapStateToProps)(OverviewDetailsTrends);
