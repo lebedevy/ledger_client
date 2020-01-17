a71f2abc618e5046506c7a6ebaf8e789646579a1
Yury Lebedev <y.d.lebedev@gmail.com>
2020-01-16 19:28:10 -0500

    Basic bar chart

--- src/components/AggregateOverview/OverviewDetailsTrends.js
+++ src/components/AggregateOverview/OverviewDetailsTrends.js
@@ -25,6 +25,7 @@ let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'O
 function OverviewDetailsTrends({ selected, type, start, end }) {
     const classes = useStyles();
     const [data, setData] = useState(null);
+    const [max, setMax] = useState(null);
 
     useEffect(() => {
         if (selected) getData();
@@ -34,16 +35,12 @@ function OverviewDetailsTrends({ selected, type, start, end }) {
         // console.log(selected);
         console.log(start);
         const currentDate = new Date(end);
-        const endDate = getFormatedDate(
-            new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0)
-        );
-        const startDate = getFormatedDate(
-            new Date(currentDate.getFullYear() - 1, currentDate.getMonth() + 1, 1)
-        );
+        const [year, month] = [currentDate.getFullYear(), currentDate.getMonth()];
+        // Get one year period
+        const endDate = getFormatedDate(new Date(year, month + 1, 0));
+        const startDate = getFormatedDate(new Date(year - 1, month + 1, 1));
         console.log(startDate, start);
         console.log(endDate, end);
-        // getFormatedDate(new Date(year, month, 1)),
-        // getFormatedDate(new Date(year, month + 1, 0)),
         const res = await fetch(
             `/api/users/expenses/overview/${type}/trends?start=${startDate}&end=${endDate}&id=${selected.el.id}`
         );
@@ -51,8 +48,35 @@ function OverviewDetailsTrends({ selected, type, start, end }) {
         if (res.ok) {
             const data = await res.json();
             console.log(data);
-            setData(data);
+            console.log(endDate.slice(5, 7));
+            const formatted = formatData(data.expenses, parseInt(endDate.slice(5, 7)) - 1);
+            setData(formatted);
+            return data;
         }
+        return null;
+    }
+
+    function formatData(data, start) {
+        let formatted = [];
+        let max = 0;
+        if (start < 0) start = 11;
+        console.log(start);
+        for (let i = 0, ind = 0; i < 12; i++) {
+            let month = i + start >= 12 ? i + start - 12 : i + start;
+            let monthItem = {};
+            // console.log(months[month]);
+            if (data[ind] && parseInt(data[ind]['txn_date'].slice(5, 7)) - 1 === month) {
+                monthItem['amount'] = data[ind]['amount'];
+                max = data[ind]['amount'] > max ? data[ind]['amount'] : max;
+                ind++;
+            } else {
+                monthItem['amount'] = 0;
+            }
+            monthItem.month = months[month];
+            formatted.push(monthItem);
+        }
+        setMax(max);
+        return formatted;
     }
 
     return (
@@ -63,21 +87,23 @@ function OverviewDetailsTrends({ selected, type, start, end }) {
             </h3>
             {data ? null : <CircularProgress style={{ alignSelf: 'center', margin: '10px' }} />}
             <svg className={classes.svg}>
-                {months.map((month, ind) => (
-                    <React.Fragment>
-                        <rect
-                            key={month + 'bar'}
-                            x={`${2 + 8 * ind}%`}
-                            y={`${90}%`}
-                            width={'6%'}
-                            height={'100%'}
-                            style={{ fill: '#00000020' }}
-                        />
-                        <text key={month + 'label'} x={`${2 + 8 * ind}%`} y={`${100}%`}>
-                            {month}
-                        </text>
-                    </React.Fragment>
-                ))}
+                {data
+                    ? data.map((el, ind) => (
+                          <React.Fragment>
+                              <rect
+                                  key={el.month + 'bar'}
+                                  x={`${2 + 8 * ind}%`}
+                                  y={`${90 - 80 * (el.amount / max)}%`}
+                                  width={'6%'}
+                                  height={`${80 * (el.amount / max)}%`}
+                                  style={{ fill: '#00000090' }}
+                              />
+                              <text key={el.month + 'label'} x={`${2 + 8 * ind}%`} y={`${100}%`}>
+                                  {el.month}
+                              </text>
+                          </React.Fragment>
+                      ))
+                    : null}
             </svg>
         </div>
     );
