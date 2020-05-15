7ed7a0851131309519361902f463ea5dab90a1e2
lebedevy <y.d.lebedev@gmail.com>
2020-05-15 09:03:43 -0400

    expense row tsx refactor + add make cell editable

--- src/components/ExpenseSummary.tsx
+++ src/components/ExpenseSummary.tsx
@@ -1,41 +1,56 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/styles';
+import React, { useState } from 'react';
 import { getCurrencyFormat } from '../utility/utility';
+import { css } from 'emotion';
 
-const useStyles = makeStyles({
-    expenseEntdy: {
-        padding: '0 1px',
-        display: 'flex',
-        bottomBorder: '1px solid #00000060',
-        '& td': {
-            minHeight: '1em',
-            padding: '2px 0',
-            flex: '1 1 0',
-            border: '1px solid #00000020',
-            minWidth: '0',
-            wordBreak: 'break-all',
-            'overflow-x': 'auto',
-        },
-        '& button': {
-            borderRadius: 0,
-            border: '1px solid #00000020',
-            minWidth: '0',
-        },
-    },
-});
+const expenseEntry = css`
+    padding: 0 1px;
+    display: flex;
+    bottom-border: 1px solid #00000060;
+    td {
+        min-height: 1em;
+        padding: 2px 0;
+        flex: 1 1 0;
+        border: 1px solid #00000020;
+        min-width: 0;
+        word-break: break-all;
+        overflow-x: auto;
+    }
+    button {
+        borderradius: 0;
+        border: 1px solid #00000020;
+        minwidth: 0;
+    }
+`;
 
-function ExpenseSummary({ el, ind, expand, exclude }) {
-    const classes = useStyles();
+interface IExpense {
+    store: any;
+    category: any;
+    amount: number;
+    date: string;
+}
+
+interface IExclude {
+    store?: string;
+    category?: string;
+    date?: string;
+}
+
+function ExpenseSummary({ el, expand, exclude }: { expand: any; el: IExpense; exclude: IExclude }) {
     return (
-        <tr className={classes.expenseEntdy} onClick={expand}>
-            <td>{`$${getCurrencyFormat(el.amount)}`}</td>
-            {!exclude || !exclude['store'] ? <td>{el.store ? el.store.store_name : ''}</td> : null}
-            {!exclude || !exclude['category'] ? (
-                <td>{el.category ? el.category.category_name : ''}</td>
-            ) : null}
-            {!exclude || !exclude['date'] ? <td>{el.date}</td> : null}
+        <tr className={expenseEntry} /*onClick={expand}*/>
+            <EditableCel content={`$${getCurrencyFormat(el.amount)}`} />
+            {!exclude?.store && <EditableCel content={el?.store?.store_name ?? ''} />}
+            {!exclude?.category && <EditableCel content={el?.category?.category_name ?? ''} />}
+            {!exclude?.date && <EditableCel content={el.date} />}
         </tr>
     );
 }
 
 export default ExpenseSummary;
+
+function EditableCel({ content }: { content: string | number }) {
+    const [editing, setEditing] = useState(false);
+    return (
+        <td onClick={() => setEditing(true)}>{editing ? <input value={content} /> : content}</td>
+    );
+}
